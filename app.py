12762d584f199f05d9f4a5f299e5066493388c99
Abderahimam <abderahim.amari@student.wsb.edu.pl>
2025-10-17 15:25:11 +0200

    Add Flask app with index and submit routes

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+from flask import Flask, render_template, request
 
+app = Flask(_name_)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    return f"""
+        <h2>Thank you, {name}!</h2>
+        <p>Your message has been received.</p>
+        <p><strong>Email:</strong> {email}</p>
+        <p><strong>Message:</strong> {message}</p>
+        <br><a href='/'>Go back</a>
+    """
+
+if _name_ == '_main_':
+    app.run(debug=True)
